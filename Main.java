67391793a41f459e9358c41c6a617db859c1714b
Takuma <takuma012011@gmail.com>
2020-05-14 12:29:25 +0900

    hanoi add

--- Main.java
+++ Main.java
@@ -3,8 +3,13 @@ package Hanoi;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-
+		int n = 4;
+		Hanoi h = new Hanoi();
+		h.initialize(n);
+		h.printPiles();
+		//n: 円盤の枚数
+		//0番の杭を二番に杭に一番の杭を経由している
+		h.hanoi(n,0,2,1);
 	}
 
 }
